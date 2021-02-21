b77572a4bde42b8d4c46a8290423b991a883b6e1
rwanyoike <raymond.wanyoike@gmail.com>
2021-02-21 13:09:59 +0300

    inital commit

---
+++ functions.sh
@@ -0,0 +1,55 @@
+# This file must be used with "source <FILE>" *from bash*. You cannot run it
+# directly
+
+ui_sgr0=$(tput sgr0)
+ui_002=$(tput setaf 2)
+ui_bold=$(tput bold)
+
+# ${1}: message
+function dotfiles_print() {
+  printf "${ui_bold}${ui_002}%s${ui_sgr0}\n" "${1}"
+}
+export -f dotfiles_print
+
+
+# ${1}: repo
+# ${2}: dest
+# ${3}: version
+function dotfiles_git() {
+  if [[ ! -d ${2} ]]; then
+    git clone "${1}" "${2}"
+  fi
+
+  pushd "${2}" >/dev/null 2>&1
+  git fetch -pPt
+  git reset --hard "origin/${3:-master}"
+  popd >/dev/null 2>&1
+}
+export -f dotfiles_git
+
+# ${1}: source
+# ${2}: dest
+function dotfiles_link() {
+  if [[ ! -h ${2} ]]; then
+    ln -s "${1}" "${2}"
+  fi
+}
+export -f dotfiles_link
+
+# ${1}: path
+function dotfiles_mkdir() {
+  if [[ ! -d ${1} ]]; then
+    mkdir -p "${1}"
+  fi
+}
+export -f dotfiles_mkdir
+
+# ${1}: files
+function dotfiles_init() {
+  find "${1}" -type f | while read -r fname; do
+    relative_path="${fname//${1}\//.}"
+    dotfiles_mkdir "${HOME}/$(dirname "${relative_path}")"
+    dotfiles_link "${fname}" "${HOME}/${relative_path}"
+  done
+}
+export -f dotfiles_init
---
+++ provision.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DOTFILES_DIR="${HOME}/.dotfiles"
+
+# Ref: http://www.binaryphile.com/bash/2020/01/12/determining-the-location-of-your-script-in-bash.html
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")"; pwd)
+
+# shellcheck source=functions.sh
+. "${HERE}/functions.sh"
+
+# symlink system_All
+if [[ -d ${DOTFILES_DIR}/dotfiles/system_All ]]; then
+  dotfiles_print "--> symlink ${DOTFILES_DIR}/dotfiles/system_All"
+  dotfiles_init "${DOTFILES_DIR}/dotfiles/system_All"
+fi
+
+# symlink system_$(uname)
+if [[ -d ${DOTFILES_DIR}/dotfiles/system_$(uname) ]]; then
+  dotfiles_print "--> symlink ${DOTFILES_DIR}/dotfiles/system_$(uname)"
+  dotfiles_init "${DOTFILES_DIR}/dotfiles/system_$(uname)"
+fi
+
+# execute dotfiles.d
+if [[ -d ${DOTFILES_DIR}.d ]]; then
+  dotfiles_print "--> execute ${DOTFILES_DIR}.d"
+  for file in "${DOTFILES_DIR}.d"/*.sh; do
+    dotfiles_print "... ./$(basename "${file}")"
+    bash "${file}" || break
+  done
+fi
